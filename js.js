5526220c8f203c4e1e3008cb81063f93f33d3dfc
Hamad-158 <Hamad.bfar@gmail.com>
2020-09-26 19:58:43 +0300

    z

---
+++ js.js
@@ -0,0 +1,18 @@
+// let age = 17
+// console.log(age) 
+// console.log(age*5)
+// console.log("حين أبلغ 5 أضعاف عمري سيبدأ زحل باستقبال المبرمجين على سطحه 🪐")
+// let numberOfYears = 1969 - 2020
+// console.log(` ${numberOfYears} هو عدد الأعوام التي مرت على صعود الإنسان على سطح القمر 🌑`)
+
+
+let pets = ["dog" , "cat" , "lion" , "loveBird" , "Bear"];
+let cat = { 
+    name : "mm" ,
+    age : 1 ,
+    emoji : "🎈" ,
+    slala : "madry"
+ }
+
+ console.log(pets[1])
+ console.log(cat.emoji)
